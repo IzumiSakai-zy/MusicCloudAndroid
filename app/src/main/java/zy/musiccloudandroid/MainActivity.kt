4ac4113eccabe3196b8e2284475fd256ad50ee00
izumisakai-zy <izumisakai-zy@outlook.com>
2022-01-21 11:49:57 -0400

    update:添加code locator依赖

--- app/src/main/java/zy/musiccloudandroid/MainActivity.kt
+++ app/src/main/java/zy/musiccloudandroid/MainActivity.kt
@@ -3,21 +3,14 @@ package zy.musiccloudandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.viewModels
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.Volley
-import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 import zy.musiccloudandroid.adapter.SongAdapter
 import zy.musiccloudandroid.const.MySingleTon
 import zy.musiccloudandroid.entity.Song
-import zy.musiccloudandroid.model.MainActivityViewModel
-import zy.musiccloudandroid.model.MainViewModelProvider
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -28,7 +21,7 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         val result = JsonObjectRequest(Request.Method.GET,"http://47.108.63.126/api/songs",null,
-            Response.Listener {
+            {
                 Log.d("MainActivity",it.toString())
                 val jsonArray = JSONObject(it.toString()).getJSONObject("data").getJSONArray("songs")
                 Log.d("MainActivity",jsonArray.toString())
@@ -42,7 +35,7 @@ class MainActivity : AppCompatActivity() {
                 songRecyclerView.adapter = SongAdapter(MySingleTon.getSongArrayList())
                 songRecyclerView.layoutManager = LinearLayoutManager(this)
             },
-            Response.ErrorListener {
+            {
                 Log.d("MainActivity",it.toString())
             })
         MySingleTon.getInstance(this).addToRequestQueue(result)
@@ -80,9 +73,5 @@ class MainActivity : AppCompatActivity() {
 
             mediaPlayer.seekTo(mediaPlayer.currentPosition + 30 * 1000)
         }
-
-
     }
-
-
 }
